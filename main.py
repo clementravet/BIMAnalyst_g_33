8139fa133c3875bba649ece1d992b6e3fb3a2d02
clementravet <clement.ravet62@gmail.com>
2024-09-16 16:10:42 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 import ifcopenshell
 
 from .rules import pipeRule
-from .rules import doorRule
+from .rules import ventilationNameRule
+from .rules import ventilationRule
 
 model = ifcopenshell.open("path/to/ifcfile.ifc")
 
-pipeResult = pipeRule.checkRule(ifc)
-doorResult = doorRule.checkRule(model)
+pipeResult = pipeRule.checkRule(model)
+ventilationNameResult = ventilationNameRule.checkRule(model)
+ventilationResult = ventilationRule.checkRule(model)
 
 print("Pipe result:", pipeResult)
-print("Door result:", doorResult)
+print("Ventilation Name result:", ventilationNameResult)
+print("Ventilation result:", ventilationResult)
