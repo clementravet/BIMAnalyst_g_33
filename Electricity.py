d2a8160ca4f379b90524a1e6982c17bda9d0920c
clementravet <clement.ravet62@gmail.com>
2024-09-16 13:34:01 +0200

    Update Electricity.py

--- Electricity.py
+++ Electricity.py
@@ -1,11 +1,10 @@
-import ifcopenshell
-import ifcopenshell.util
-import ifcopenshell.util.element
-import ifcopenshell.util.selector
+#import ifcopenshell
+#import ifcopenshell.util
+#import ifcopenshell.util.element
+#import ifcopenshell.util.selector
 
-ifc = ifcopenshell.open('C:\Users\clemr\Desktop\DTU\h-----COURSES-----h\41934-Advanced BIM\elec.ifc')
+#ifc = ifcopenshell.open('C:\Users\clemr\Desktop\DTU\h-----COURSES-----h\41934-Advanced BIM\elec.ifc')
 ###selector = Selector()
-
 
 #things = file.by_type('IfcLightFixtureType')
 ###things = ifcopenshell.util.selector.parse(ifc, '.IfcLightFixtureType')
@@ -21,7 +20,15 @@ ifc = ifcopenshell.open('C:\Users\clemr\Desktop\DTU\h-----COURSES-----h\41934-Ad
 #element = ifcopenshell.util.selector.parse(file, '.IfcLightFixtureType[IfcElectricVoltageMeasure = "220."]')
 #print(element)
 
-
-
 #Pset_ElectricalDeviceCommon
 #IfcElectricVoltageMeasure
+
+
+
+
+
+import ifcopenshell
+model = ifcopenshell.open('C:\Users\clemr\Desktop\DTU\elec.ifc')
+
+lights = model.by_type('IfcLightFixtureType')
+print(f"Lights in model: {len(lights)}")
