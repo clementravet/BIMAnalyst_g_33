50a467e7c459273fc56e0e628f26986f00b15f0a
clementravet <clement.ravet62@gmail.com>
2024-09-16 14:44:00 +0200

    Update Electricity.py

--- Electricity.py
+++ Electricity.py
@@ -1,34 +1,28 @@
-#import ifcopenshell
-#import ifcopenshell.util
-#import ifcopenshell.util.element
-#import ifcopenshell.util.selector
-
-#ifc = ifcopenshell.open('C:\Users\clemr\Desktop\DTU\h-----COURSES-----h\41934-Advanced BIM\elec.ifc')
-###selector = Selector()
-
-#things = file.by_type('IfcLightFixtureType')
-###things = ifcopenshell.util.selector.parse(ifc, '.IfcLightFixtureType')
-###Elecapp = things[0]
-
-###print(len(things))
-
-#for thing in things:
-#    print(thing.Name)
-
-#print(ifcopenshell.util.element.get_psets(Elecapp))
-
-#element = ifcopenshell.util.selector.parse(file, '.IfcLightFixtureType[IfcElectricVoltageMeasure = "220."]')
-#print(element)
-
-#Pset_ElectricalDeviceCommon
-#IfcElectricVoltageMeasure
-
-
-
-
-
+####################################################################################################################################
+#############################################          IMPORT WINDOW          ######################################################
+####################################################################################################################################
 import ifcopenshell
-model = ifcopenshell.open('C:\Users\clemr\Desktop\DTU\elec.ifc')
+import ifcopenshell.util
+import ifcopenshell.util.element
+import ifcopenshell.util.selector
 
-lights = model.by_type('IfcLightFixtureType')
-print(f"Lights in model: {len(lights)}")
+model = ifcopenshell.open(r"C:\Users\clemr\Desktop\DTU\-----COURSES-----\41934-AdvancedBIM\elec.ifc")
+
+
+#lights = model.by_type('IfcLightFixtureType')
+#print(f"Lights in model: {len(lights)}")
+
+LightFixture = model.by_type('IfcLightFixtureType')
+FirstLightFixture = LightFixture[0]
+
+
+####################################################################################################################################
+#############################################          PRINT WINDOW          ######################################################
+####################################################################################################################################
+print(len(LightFixture))
+print(ifcopenshell.util.element.get_psets(FirstLightFixture))
+#print(ifcopenshell.util.selector.filter_elements(model, "IfcLightFixtureType"))
+
+
+
+
