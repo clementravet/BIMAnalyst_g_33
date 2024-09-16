ccd295fba0c9b95b6aae4c928d5e2284bf70b1a4
clementravet <clement.ravet62@gmail.com>
2024-09-16 15:56:33 +0200

    Update Electricity.py

--- Electricity.py
+++ Electricity.py
@@ -1,28 +1,77 @@
 ####################################################################################################################################
 #############################################          IMPORT WINDOW          ######################################################
 ####################################################################################################################################
-import ifcopenshell
-import ifcopenshell.util
-import ifcopenshell.util.element
-import ifcopenshell.util.selector
+#import ifcopenshell
+#import ifcopenshell.util
+#import ifcopenshell.util.element
+#import ifcopenshell.util.selector
 
-model = ifcopenshell.open(r"C:\Users\clemr\Desktop\DTU\-----COURSES-----\41934-AdvancedBIM\elec.ifc")
+#model = ifcopenshell.open(r"C:\Users\clemr\Desktop\DTU\-----COURSES-----\41934-AdvancedBIM\elec.ifc")
 
 
 #lights = model.by_type('IfcLightFixtureType')
 #print(f"Lights in model: {len(lights)}")
 
-LightFixture = model.by_type('IfcLightFixtureType')
-FirstLightFixture = LightFixture[0]
+#LightFixture = model.by_type('IfcLightFixtureType')
+#FirstLightFixture = LightFixture[0]
 
 
 ####################################################################################################################################
 #############################################          PRINT WINDOW          ######################################################
 ####################################################################################################################################
-print(len(LightFixture))
-print(ifcopenshell.util.element.get_psets(FirstLightFixture))
+#print(len(LightFixture))
+#print(ifcopenshell.util.element.get_psets(FirstLightFixture))
 #print(ifcopenshell.util.selector.filter_elements(model, "IfcLightFixtureType"))
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+####################################################################################################################################
+#############################################          IMPORT WINDOW          ######################################################
+####################################################################################################################################
+import ifcopenshell
+import ifcopenshell.util
+import ifcopenshell.util.element
+import ifcopenshell.util.selector
+
+model = ifcopenshell.open(r"C:\Users\clemr\Desktop\DTU\-----COURSES-----\41934-AdvancedBIM\Assignment 1\CES_BLD_24_06_MEP.ifc")
+
+MechanicalVentilation = model.by_type("IfcBuildingElementProxy")
+MechanicalVentilation1 = MechanicalVentilation[0]
+MechanicalVentilation2 = MechanicalVentilation[1]
+
+#### Number of elements for the mechanical ventilation
+print(f"There are {len(MechanicalVentilation)} mechanical ventilation systems")
+print("     ")
+
+#### Name of Mechanical ventilation
+print(f"The first mechanical ventilation is {MechanicalVentilation1.Name}")
+print(f"The second mechanical ventilation is {MechanicalVentilation2.Name}")
+print("     ")
+
+#### 
+print(ifcopenshell.util.selector.filter_elements(model, "IfcBuildingElementProxy"))
+
+####################################################################################################################################
+##############################################          PRINT WINDOW          ######################################################
+####################################################################################################################################
+#print(ifcopenshell.util.element.get_psets(MechanicalVentilation))
+#print(ifcopenshell.util.selector.filter_elements(model, "IfcLightFixtureType"))
 
 
