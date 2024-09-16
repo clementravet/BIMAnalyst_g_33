290026fc69fc8677d109f53b1254dac31b2e0838
clementravet <clement.ravet62@gmail.com>
2024-09-16 13:20:08 +0200

    Update Electricity.py

--- Electricity.py
+++ Electricity.py
@@ -1,1 +1,27 @@
 import ifcopenshell
+import ifcopenshell.util
+import ifcopenshell.util.element
+import ifcopenshell.util.selector
+
+ifc = ifcopenshell.open('C:\Users\clemr\Desktop\DTU\h-----COURSES-----h\41934-Advanced BIM\elec.ifc')
+###selector = Selector()
+
+
+#things = file.by_type('IfcLightFixtureType')
+###things = ifcopenshell.util.selector.parse(ifc, '.IfcLightFixtureType')
+###Elecapp = things[0]
+
+###print(len(things))
+
+#for thing in things:
+#    print(thing.Name)
+
+#print(ifcopenshell.util.element.get_psets(Elecapp))
+
+#element = ifcopenshell.util.selector.parse(file, '.IfcLightFixtureType[IfcElectricVoltageMeasure = "220."]')
+#print(element)
+
+
+
+#Pset_ElectricalDeviceCommon
+#IfcElectricVoltageMeasure
