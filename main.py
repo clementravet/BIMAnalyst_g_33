65da887746312061e7325665e1023fb528240ec6
clementravet <clement.ravet62@gmail.com>
2024-11-11 14:17:49 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,31 @@
 import ifcopenshell
 
-from .rules import pipeRule
-from .rules import ventilationNameRule
-from .rules import ventilationRule
+#from .rules import Finalproject
+from .rules import Finalproject
 
-model = ifcopenshell.open("path/to/ifcfile.ifc")
+#model = ifcopenshell.open("path/to/ifcfile.ifc")
+model = ifcopenshell.open(r"C:\Users\clemr\Desktop\DTU\-----COURSES-----\41934-AdvancedBIM\Assignment 1\CES_BLD_24_06_ARC.ifc")
 
-pipeResult = pipeRule.checkRule(model)
-ventilationNameResult = ventilationNameRule.checkRule(model)
-ventilationResult = ventilationRule.checkRule(model)
 
-print("Pipe result:", pipeResult)
-print("Ventilation Name result:", ventilationNameResult)
-print("Ventilation result:", ventilationResult)
+U_value_curtain_walls = Finalproject.U_value_curtain_walls(model)
+print("The different U_values coming from the IfcCurtainWall are :", U_value_curtain_walls)
+print(tabulate(U_value_curtain_walls, tablefmt="grid"))
+print(" ")
+
+U_value_walls = Finalproject.U_value_walls(model)
+print("The different U_values coming from the IfcWall are :", U_value_walls)
+print(tabulate(U_value_walls, tablefmt="grid"))
+print(" ")
+
+U_value_roofs = Finalproject.U_value_roofs(model)
+print("The different U_values coming from the IfcRoof are :", U_value_roofs)
+print(tabulate(U_value_roofs, tablefmt="grid"))
+print(" ")
+
+U_value_slabs = Finalproject.U_value_slabs(model)
+print("The different U_values coming from the IfcSlab are :", U_value_slabs)
+print(tabulate(U_value_slabs, tablefmt="grid"))
+print(" ")
+
+
+
